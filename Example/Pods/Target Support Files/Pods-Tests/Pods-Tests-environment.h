a15e762e47dcd19d62c046b9e9d5a358bf07aaa0
Joe Noon <joenoon@gmail.com>
2014-10-08 23:48:20 -0700

    cocoapods stuff

---
+++ Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Firebase-RACExtensions
+#define COCOAPODS_POD_AVAILABLE_Firebase_RACExtensions
+#define COCOAPODS_VERSION_MAJOR_Firebase_RACExtensions 0
+#define COCOAPODS_VERSION_MINOR_Firebase_RACExtensions 1
+#define COCOAPODS_VERSION_PATCH_Firebase_RACExtensions 0
+
